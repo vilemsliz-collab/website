a7fce24904893ef978a1466d22eec6bd2b81fe72
Vilém Slíž <vilemsliz@Vilem--MacBook-Air-8.local>
2026-05-10 00:39:27 +0200

    feat: add Vilém Slíž headline top-aligned in bio card

--- components/case-study/BioPairCard.tsx
+++ components/case-study/BioPairCard.tsx
@@ -3,6 +3,7 @@ import s from './BioPairCard.module.css'
 export default function BioPairCard() {
   return (
     <div className={s.card}>
+      <h1 className={s.name}>Vilém Slíž</h1>
       <p className={s.bio}>
         I'm a generalist visual designer focused on brand systems. I build seamless brand experiences from top of the funnel to product in the B2B SaaS space at Wrike. I care about turning brand strategy into systems people actually enjoy using.
       </p>
