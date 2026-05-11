8f5c39be15356757bcba386a60c3aad0849cde66
Vilém Slíž <vilemsliz@Vilem--MacBook-Air-8.local>
2026-05-12 01:04:18 +0200

    Revert "OrbBackground: physics throttle, hidden skip, responsive scale"
    This reverts commit b80c68bceab668359a50a5c363628c297eae4f09.

--- components/loader/Loader.tsx
+++ components/loader/Loader.tsx
@@ -6,7 +6,7 @@ export default function Loader({ visible }: { visible: boolean }) {
   return (
     <div className={`${s.loader} ${visible ? '' : s.hidden}`} aria-hidden={!visible}>
       <div className={s.orbWrap}>
-        <OrbBackground numOrbs={5} orbScale={1.5} responsive />
+        <OrbBackground numOrbs={5} orbScale={2} />
       </div>
       <p className={s.name}>Vilem</p>
     </div>
