ca4af8da4121ad1894cf09d80dadf9eded11b5f8
Vilém Slíž <vilemsliz@Vilem--MacBook-Air-8.local>
2026-05-12 23:00:15 +0200

    Revert "about card: render name as a plain text node in the headline"
    This reverts commit 234155d39b5ea1c0585c1e1e2979571ddc54761e.

--- components/carousel/CarouselDOMScene.tsx
+++ components/carousel/CarouselDOMScene.tsx
@@ -383,7 +383,7 @@ export default function CarouselDOMScene({
                   return (
                     <p className={styles.cardAboutHeadline}>
                       {lines[0]}
-                      {card.name && (<><br />{card.name}</>)}
+                      {card.name && (<><br /><span className={styles.cardAboutHeadlineName}>{card.name}</span></>)}
                       {lines.slice(1).map((line, li) => (
                         <span key={li}><br />{line}</span>
                       ))}
